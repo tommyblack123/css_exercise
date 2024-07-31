279d078a33d539fdb0765b648f0e477da5d1c7f3
tomcat <tomcat>
2024-07-31 14:56:05 +0800

    111

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -4,19 +4,28 @@ import { ref, reactive, onMounted } from 'vue';
 
 <template>
 
-        <ul class="box">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
+
+    <ul class="box">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+    </ul>
 
 </template>
 
 <style scoped lang='scss'>
     .box{
+        width: 500px;
+        height: 500px;
 display:grid;
-grid-template-columns: 100px 200px 100px;
+grid-template-columns: repeat(3,1fr);
+grid-template-rows: repeat(3,1fr);
 
     }
 
